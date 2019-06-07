1fc748d2abbe06f2177d811cedec0f221f501ae4
lialee <46589495+hwaria@users.noreply.github.com>
2019-06-07 16:57:05 +0900

    Create main.java

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("사람1", new Person("고경환", 45));
+		map.put("사람2", new Person("문승진", 33));
+		map.put("사람3", new Person("변자룡", 32));
+		map.put("사람4", new Person("이창준", 31));
+		map.put("사람5", new Person("김수용", 55));
+		
+		
+		
+		System.out.println(map);
+		
+	}
+}
